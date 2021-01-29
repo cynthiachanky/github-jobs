3048663951882b9c13df2f4ae6e8dc8728ddc5f7
Cynthia Chan <cynthiachanky@gmail.com>
2021-01-29 16:44:39 +0800

    UPDATE: SearchCard.js

--- components/SearchCard.js
+++ components/SearchCard.js
@@ -1,24 +1,34 @@
+import {useState} from 'react';
 import {Button, Card, CardImg, CardImgOverlay, Input, InputGroup, InputGroupAddon, InputGroupText} from 'reactstrap';
 
-const SearchCard = () => (
-  <Card>
-    <CardImg src={'/background-image.png'} alt={'Background Image'}/>
-    <CardImgOverlay>
-      <InputGroup>
-        <InputGroupAddon addonType={'prepend'}>
-          <InputGroupText>
-            <i className={'material-icons'}>work_outline</i>
-          </InputGroupText>
-        </InputGroupAddon>
+const SearchCard = ({callback}) => {
+  const [inputValue, setInputValue] = useState('');
 
-        <Input placeholder={'Title, companies, expertise or benefits'}/>
+  const onChange = e => setInputValue(e.target.value);
 
-        <InputGroupAddon addonType={'append'}>
-          <Button>Search</Button>
-        </InputGroupAddon>
-      </InputGroup>
-    </CardImgOverlay>
-  </Card>
-);
+  const onClick = () => callback(inputValue);
+
+  return (
+    <Card className={'search-card'}>
+      <CardImg src={'/background-image.png'} alt={'Background Image'}/>
+
+      <CardImgOverlay>
+        <InputGroup>
+          <InputGroupAddon addonType={'prepend'}>
+            <InputGroupText>
+              <i className={'material-icons'}>work_outline</i>
+            </InputGroupText>
+          </InputGroupAddon>
+
+          <Input placeholder={'Title, companies, expertise or benefits'} value={inputValue} onChange={onChange}/>
+
+          <InputGroupAddon addonType={'append'}>
+            <Button onClick={onClick}>Search</Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </CardImgOverlay>
+    </Card>
+  );
+};
 
 export default SearchCard;
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,24 @@ import SearchCard from '../components/SearchCard';
 import FilterForm from '../components/FilterForm';
 import JobCard from '../components/JobCard';
 
-const IndexPage = () => (
-  <>
-    <SearchCard/>
-    <Row>
-      <Col md={5}>
-        <FilterForm/>
-      </Col>
-      <Col md={7}>
-        <JobCard/>
-      </Col>
-    </Row>
-  </>
-);
+const IndexPage = () => {
+  const handleSearch = keyword => {
+    console.log('[handleSearch]', keyword);
+  };
+
+  return (
+    <>
+      <SearchCard callback={handleSearch}/>
+      <Row>
+        <Col md={5}>
+          <FilterForm/>
+        </Col>
+        <Col md={7}>
+          <JobCard/>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default IndexPage;
