e189d9658c6df80aacb501095a8a321e13269812
Cynthia Chan <cynthiachanky@gmail.com>
2021-01-29 18:33:41 +0800

    UPDATE: JobCard.js

--- components/JobCard.js
+++ components/JobCard.js
@@ -1,13 +1,35 @@
-import {Badge, Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle} from 'reactstrap';
+import {Badge, Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle, Col, Row} from 'reactstrap';
 
-const JobCard = () => (
-  <Card>
+const JobCard = ({id, company_logo, company, title, location, created_at, type}) => (
+  <Card className={'job-card'}>
     <CardBody>
-      <CardImg src={'/devchallenges.png'} alt={'Logo'}/>
-      <CardSubtitle>Company</CardSubtitle>
-      <CardTitle>Position</CardTitle>
-      <Badge>Full Time</Badge>
-      <CardText>Location Posting Time</CardText>
+      <Row>
+        <Col sm={3} xl={2}>
+          <CardImg src={company_logo} alt={company}/>
+        </Col>
+
+        <Col sm={9} xl={10}>
+          <CardSubtitle>{company}</CardSubtitle>
+          <CardTitle>{title}</CardTitle>
+          <Row>
+            <Col sm={12} md={4}>
+              {type === 'Full Time' && <Badge color={'dark'}>Full Time</Badge>}
+            </Col>
+            <Col sm={6} md={4}>
+              <CardText>
+                <i className={'material-icons'}>public</i>
+                {location}
+              </CardText>
+            </Col>
+            <Col sm={6} md={4}>
+              <CardText>
+                <i className={'material-icons'}>schedule</i>
+                {created_at}
+              </CardText>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </CardBody>
   </Card>
 );
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,27 @@ import SearchCard from '../components/SearchCard';
 import FilterForm from '../components/FilterForm';
 import JobCard from '../components/JobCard';
 
+const jobList = [
+  {
+    id: 'devchallenges-job-1',
+    company_logo: '/devchallenges.png',
+    company: 'Github',
+    title: 'Challenger',
+    location: 'The Earth',
+    created_at: 'Thu Jan 28 16:50:15 UTC 2021',
+    type: 'Full Time'
+  },
+  {
+    id: 'devchallenges-job-2',
+    company_logo: '/devchallenges.png',
+    company: 'Github',
+    title: 'Challenger',
+    location: 'The Earth',
+    created_at: 'Thu Jan 28 16:50:15 UTC 2021',
+    type: 'Part Time'
+  }
+];
+
 const IndexPage = () => {
   const handleSearch = keyword => {
     console.log('[handleSearch]', keyword);
@@ -16,11 +37,11 @@ const IndexPage = () => {
     <>
       <SearchCard callback={handleSearch}/>
       <Row className={'my-4'}>
-        <Col md={5}>
+        <Col lg={5}>
           <FilterForm callback={handleFilter}/>
         </Col>
-        <Col md={7}>
-          <JobCard/>
+        <Col lg={7}>
+          {jobList.map(job => <JobCard key={job.id} {...job}/>)}
         </Col>
       </Row>
     </>
